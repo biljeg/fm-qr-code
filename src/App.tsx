56e40325373182ac09feb2acc0801680e934d0b7
biljeg <brunosiljeg@outlook.com>
2024-08-07 21:37:38 +0200

    feat: finished component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import QRCodeImage from "./assets/images/image-qr-code.png";
+
 function App() {
   return (
-    <>
-      <h4>Improve your front-end skills by building projects</h4>
-      <p>
-        Scan the QR code to visit Frontend Mentor and take your coding skills to
-        the next level
-      </p>
-    </>
+    <main className="flex items-center justify-center w-screen min-h-screen">
+      <section className="flex flex-col px-4 pt-4 pb-10 max-w-80 bg-pure-white rounded-[20px] gap-6 shadow-xl">
+        <div className="rounded-[10px]">
+          <img
+            className="rounded-[10px] h-auto max-w-full"
+            src={QRCodeImage}
+            alt="scannable qr code"
+          />
+        </div>
+        <div className="flex flex-col gap-4 px-4 text-center">
+          <h4 className="text-lg font-bold text-dark-slate">
+            Improve your front-end skills by building projects
+          </h4>
+          <p className="text-base tracking-wide text-light-slate">
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
+      </section>
+    </main>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontSize: {
+      base: ["0.9375rem", "1.4"],
+      lg: ["1.375rem", "1.2"],
+    },
+    letterSpacing: {
+      wide: "0.0125em",
+    },
+    boxShadow: {
+      xl: "0px 25px 25px 0px rgba(0, 0, 0, 0.05)",
+    },
     extend: {
       colors: {
         "pure-white": "#ffffff",
         "light-slate": "#68778d",
         "dark-slate": "#1f314f",
-        "pattens-blue": "#1f314f",
+        "pattens-blue": "#D5E1EF",
       },
     },
   },
